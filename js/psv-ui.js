a16e8e9e63d1f954de85e0e027af949b07afb84f
iyinchao <charles.yin.chao@gmail.com>
2013-10-15 22:51:30 +0800

    UI work

--- js/psv-ui.js
+++ js/psv-ui.js
@@ -7,7 +7,7 @@ function boot() {
 	console.log("boot...");
 	bootSpin = document.createElement("div");
 	titleLogo = document.createElement("img");
-	document.body.appendChild(bootSpin);
+	//document.body.appendChild(bootSpin);
 	bootSpin.id = "boot_spin";
 	bootSpin.class = "round";
 	var xhr;
@@ -16,14 +16,13 @@ function boot() {
 	} else {
 		xhr = new ActiveXObject("Microsoft.XMLHTTP");
 	}
-	xhr.responseType = "blob";
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && xhr.status == 200) {
 			titleLogo.src = "resources/title.png";
 			titleLogo.id = "title_logo";
 			titleLogo.style.display = "none";
 			$("body").prepend(titleLogo);
-			$("#title_logo").fadeIn();
+			$(titleLogo).fadeIn(1000);
 		}
 	}
 	xhr.open("GET", "resources/title.png", true);
